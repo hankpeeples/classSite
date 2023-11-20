76f2bce5ebba72fee2527b5932bd8c0e2792304f
Henry Peeples <hgpeeples48@gmail.com>
2023-11-19 18:27:07 -0600

    update client

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
-    },
-    api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'us-east-2_ihdCZZRbv', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '3v4mfm7dnidptpsgqr3u1kp6d9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'us-east-2',
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
